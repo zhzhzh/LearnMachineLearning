898bc1b40fad6eed1cff84e1927e0f1156dd5a79
Zhang, Jie <jzhang14@paypal.com>
2018-05-19 23:31:13 +0800

    update ex3 - done part of

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -37,11 +37,12 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X * theta);
+theta1 = [0; theta(2:end)];
 
+J = 1/m * ((-y)' * log(h) - (1 - y)' * log(1 - h)) + lambda/ (2*m) * theta1'*theta1;
 
-
-
-
+grad = 1/m * X' * (h - y) + lambda/m * theta1;
 
 
 
